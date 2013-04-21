6b38312c8a03564f51cb3994f354fbaeb30b9964
Nik Bauer <njb26@case.edu>
2013-04-21 10:14:21 -0400

    Enemies can "see" allies in given sightRange
    Just assign an int (sightRange) around enemies if you want them to be
    able to see you.
    
    Enemies scan in all directions around them.

--- Project4/Assets/EnemyMovementScript.cs
+++ Project4/Assets/EnemyMovementScript.cs
@@ -8,12 +8,15 @@ public class EnemyMovementScript : MonoBehaviour {
 	//for simplicity, the game will not have the baddies patrol
 	//a path that is a combination of these
 	public float walkRange; //how far rom the base position they walk
-	public int sightRange;//how far they can "see"
-	Vector3 basePos; //initial position they are on the map
-	float moveStep = 100.0f;//at a rate 1 = 1 sec, make a move each moveStep
-	float moveTime;
 	Vector3 negLimit;
 	Vector3 posLimit;
+	public int sightRange;//how far they can "see"
+	Vector3 negSightLimit;
+	Vector3 posSightLimit;
+	Vector3 basePos; //initial position they are on the map
+	//float moveStep = 100.0f;//at a rate 1 = 1 sec, make a move each moveStep
+	float moveTime;
+	
 	//if horizontal, false means walk left, true is walk right
 	//if vertical, false is walk down, true is walk up
 	bool walkDirection;
@@ -25,6 +28,10 @@ public class EnemyMovementScript : MonoBehaviour {
 		moveTime = 0.0f;
 		walkDirection = false;
 		//set limit on positions for patrolling behavior
+		setPatrolLimit();
+	}
+	
+	void setPatrolLimit(){
 		negLimit = transform.position;
 		posLimit = transform.position;
 		if(horizontal){
@@ -32,18 +39,23 @@ public class EnemyMovementScript : MonoBehaviour {
 			posLimit.x = basePos.x - walkRange*10;
 		}
 		else if(vertical){
-			negLimit.z = basePos.z + walkRange;
-			posLimit.z = basePos.z + walkRange;
+			negLimit.z = basePos.z + walkRange*10;
+			posLimit.z = basePos.z - walkRange*10;
 		}
 		else{
 			print ("Error: horizontal or vertical movement not specified");
 		}
 	}
-	
+	int count = 0;
 	// Update is called once per frame
 	void Update () {
+		count++;
 		moveTime += Time.deltaTime;
 		if(moveTime >=1.0f){
+			//sets the positions it can "see"
+			setSightLimit();
+			//scans those positions for allies
+			scanForAllies();			
 			if(horizontal){
 				horizontalMove();
 			}
@@ -52,6 +64,17 @@ public class EnemyMovementScript : MonoBehaviour {
 			}
 			moveTime = 0;
 		}	
+	}
+	
+	void setSightLimit(){
+		negSightLimit = transform.position;
+		posSightLimit = transform.position;
+		
+		negSightLimit.x = transform.position.x + sightRange*10;
+		negSightLimit.z = transform.position.z + sightRange*10;
+		
+		posSightLimit.x = transform.position.x - sightRange*10;
+		posSightLimit.z = transform.position.z - sightRange*10;
 	}
 	
 	void horizontalMove(){
@@ -101,6 +124,25 @@ public class EnemyMovementScript : MonoBehaviour {
 			}
 		}
 	
+	}
+	
+	bool scanForAllies(){
+		//get all Allies
+		GameObject[] allies;
+		allies = GameObject.FindGameObjectsWithTag("Player");
+		
+		foreach(GameObject obj in allies){
+			float xVal = obj.transform.position.x;
+			float zVal = obj.transform.position.z;
+			//see if inside the horizontal part of sightRange
+			if(xVal >= posSightLimit.x && xVal <= negSightLimit.x){
+				//check if inside the vertical sightRange
+				if(zVal >= posSightLimit.z && zVal <= negSightLimit.z){
+					return true;
+				}			
+			}	
+		}
+		return false;	
 	}
 
 }
