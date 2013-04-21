614ff1d88f4a349c7a2f4dfda0c6aeefd94ee5c7
Nik Bauer <njb26@case.edu>
2013-04-21 09:41:23 -0400

    Enemies patrol
    Bool value horizontal or vertical, pick ONE when you place it on the map
    give it an int WalkRange (in the editor window) and it won't go more
    than that number of steps either left/right  or up/down

--- Project4/Assets/EnemyMovementScript.cs
+++ Project4/Assets/EnemyMovementScript.cs
@@ -12,7 +12,8 @@ public class EnemyMovementScript : MonoBehaviour {
 	Vector3 basePos; //initial position they are on the map
 	float moveStep = 100.0f;//at a rate 1 = 1 sec, make a move each moveStep
 	float moveTime;
-	
+	Vector3 negLimit;
+	Vector3 posLimit;
 	//if horizontal, false means walk left, true is walk right
 	//if vertical, false is walk down, true is walk up
 	bool walkDirection;
@@ -23,6 +24,20 @@ public class EnemyMovementScript : MonoBehaviour {
 		basePos = transform.position;	
 		moveTime = 0.0f;
 		walkDirection = false;
+		//set limit on positions for patrolling behavior
+		negLimit = transform.position;
+		posLimit = transform.position;
+		if(horizontal){
+			negLimit.x = basePos.x + walkRange*10;
+			posLimit.x = basePos.x - walkRange*10;
+		}
+		else if(vertical){
+			negLimit.z = basePos.z + walkRange;
+			posLimit.z = basePos.z + walkRange;
+		}
+		else{
+			print ("Error: horizontal or vertical movement not specified");
+		}
 	}
 	
 	// Update is called once per frame
@@ -35,6 +50,7 @@ public class EnemyMovementScript : MonoBehaviour {
 			else if(vertical){
 				verticalMove();
 			}
+			moveTime = 0;
 		}	
 	}
 	
@@ -46,7 +62,7 @@ public class EnemyMovementScript : MonoBehaviour {
 			newPos.x +=10;
 			transform.position = newPos;
 			//check if at max range, if so, next time it walks right
-			if(transform.position.x == basePos.x + walkRange){
+			if(transform.position == negLimit){
 				walkDirection = false;
 			}
 		}
@@ -56,13 +72,34 @@ public class EnemyMovementScript : MonoBehaviour {
 			newPos.x -=10;
 			transform.position = newPos;
 			//check if at max range, if so, next time it walks left
-			if(transform.position.x == basePos.x - walkRange){
+			if(transform.position == posLimit){
 				walkDirection = true;
 			}
 		}
 	}
 	
 	void verticalMove(){
+		Vector3 newPos;
+		if(walkDirection){//if it should walk down
+			//move a step down
+			newPos = transform.position;
+			newPos.z +=10;
+			transform.position = newPos;
+			//check if at max range, if so, next time it walks up
+			if(transform.position == negLimit){
+				walkDirection = false;
+			}
+		}
+		else{//it should walk up
+			//move a step up
+			newPos = transform.position;
+			newPos.z -=10;
+			transform.position = newPos;
+			//check if at max range, if so, next time it walks down
+			if(transform.position == posLimit){
+				walkDirection = true;
+			}
+		}
 	
 	}
 
