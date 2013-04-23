97549e4acb6575dabfa443fa20a06c6967790798
Nik Bauer <njb26@case.edu>
2013-04-23 18:31:56 -0400

    Leader is a brighter color
    So the user knows which character is in front/leading, it is a brighter
    color than the other two.

--- Project4/Assets/CharBehaviorScript.cs
+++ Project4/Assets/CharBehaviorScript.cs
@@ -13,9 +13,16 @@ public class CharBehaviorScript : MonoBehaviour {
 	void Start () {
 	
 	}
-	
+	Color newCol = new Color(.9f,.9f,.9f,1f);
+	Color standardCol = new Color(1f,1f,1f,1f);
 	// Update is called once per frame
 	void Update () {
+		if(charPos == 0 || charPos == 1){
+			transform.renderer.material.color = newCol;		
+		}
+		else{
+			transform.renderer.material.color = standardCol;
+		}
 	
 	}
 }
